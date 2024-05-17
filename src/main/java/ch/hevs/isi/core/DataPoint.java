c5108c73db52cf0f0f791b14092d8ba9f9577dfb
rom1ferrari <romain.ferrari@students.hevs.ch>
2024-05-17 16:22:11 +0200

    Commit DataPoint.java 17.05.2024

--- src/main/java/ch/hevs/isi/core/DataPoint.java
+++ src/main/java/ch/hevs/isi/core/DataPoint.java
@@ -1,5 +1,23 @@
 package ch.hevs.isi.core;
 
-public class DataPoint {
-    String Label;
+public abstract class DataPoint {
+    protected String label;
+    protected Boolean isOutput;
+    protected DataPoint(String label, boolean isOutput) {
+        this.label = label;
+        this.isOutput = isOutput;
+    }
+    protected void update(boolean isNewValue) {
+
+    }
+    public static DataPoint getDataPointFromLabel(String label) {
+        return
+    }
+    public String getLabel() {
+        return label;
+    }
+    public Boolean isOutput() {
+        return isOutput;
+    }
+
 }
