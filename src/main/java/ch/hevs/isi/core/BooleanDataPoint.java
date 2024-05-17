e41f09d4914f01353dadb3733b950098385aac8a
Quentin Morel <quentin.morel@students.hevs.ch>
2024-05-17 16:14:18 +0200

    modification classe BooleanDataPoint.java

--- src/main/java/ch/hevs/isi/core/BooleanDataPoint.java
+++ src/main/java/ch/hevs/isi/core/BooleanDataPoint.java
@@ -1,6 +1,23 @@
 package ch.hevs.isi.core;
 
-public class BooleanDataPoint {
+public class BooleanDataPoint extends DataPoint {
     private boolean value;
 
+    public BooleanDataPoint(String label, boolean isOuput){
+        super(label, isOutput);
+    }
+
+    public void setValue(boolean value){
+        this.value = value;
+    }
+
+    public boolean getValue(){
+        return value;
+    }
+
+    public String toString(){
+        return "label = " + label
+                + "isOutput = " + isOutput
+                + "value = " + value;
+    }
 }
