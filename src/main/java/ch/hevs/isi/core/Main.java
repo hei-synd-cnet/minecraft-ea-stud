b6fd659912cdbfe1659be9ae23427a3cb78450c2
Quentin Morel <quentin.morel@students.hevs.ch>
2024-05-24 13:41:32 +0200

    beaucoup de trucs

--- src/main/java/ch/hevs/isi/core/Main.java
+++ src/main/java/ch/hevs/isi/core/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args){
         BooleanDataPoint d1 = new BooleanDataPoint("toto", false);
         d1.setValue(true);
-        System.out.println(d1.toString());
     }
 
 }
